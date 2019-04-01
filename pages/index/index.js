aa47ab3a413a43aa545c2133c7dc967823de6d1a
Борис Южаков <boris.yuzhakov@altarix.ru>
2019-04-01 16:10:14 +0300

    added static files and sass

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,8 @@
 import Layout from '../../components/Layout'
+import './styles.scss'
 
 export default () => (
     <Layout>
-        <p>Hello Next.js!</p>
+        <p className="example">Hello Next.js!</p>
     </Layout>
 )
